821032c8cb3f35544b943a44f17583abe5da525f
Ismaelp96 <ismael6991@live.com>
2024-04-24 17:04:57 -0300

    Admin Projects Pages #3

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -10,7 +10,7 @@ export default function AdminLayout({
       <Nav>
         <NavLink href="/">Dashboard</NavLink>
         <NavLink href="/admin/products">Products</NavLink>
-        <NavLink href="/admin/users">Customer</NavLink>
+        <NavLink href="/admin/users">Customers</NavLink>
         <NavLink href="/admin/orders">Sales</NavLink>
       </Nav>
       <div className="container my-6">{children}</div>
