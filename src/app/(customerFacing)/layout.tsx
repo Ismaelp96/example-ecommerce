0a6347179d4a8813b693de422bc33f1f88a9fc81
Ismaelp96 <ismael6991@live.com>
2024-04-25 10:40:44 -0300

    Customer Home Page #5

--- src/app/(customerFacing)/layout.tsx
+++ src/app/(customerFacing)/layout.tsx
@@ -1,17 +1,18 @@
+
+
 import { Nav, NavLink } from "@/components/Nav";
 
 export const dynamic = "force-dynamic";
 
-export default function Layout({
+export default function AdminLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <>
       <Nav>
-        <NavLink href="/">Dashboard</NavLink>
-        <NavLink href="/admin/products">Products</NavLink>
-        <NavLink href="/admin/users">Customers</NavLink>
-        <NavLink href="/admin/orders">Sales</NavLink>
+        <NavLink href="/">Home</NavLink>
+        <NavLink href="/products">Products</NavLink>
+        <NavLink href="/orders">My Orders</NavLink>
       </Nav>
       <div className="container my-6">{children}</div>
     </>
